a714a5377843ec2f67b0fcf88b10e247924965a0
Kyoungeun JO <hke3259@naver.com>
2022-09-19 00:45:33 +0200

    Feat: making pages

---
+++ src/components/pages/Home.js
@@ -0,0 +1,43 @@
+import image from '../../images/image_home.png';
+import { useState, useEffect } from 'react';
+import Card from '../Card'
+import { Link } from "react-router-dom";
+import './Home.css'
+
+function Home() {
+
+    const [accomodations, setAccomodations] = useState([])
+
+    useEffect(() => {
+        fetch('/logements.json', 
+            {headers: 
+                {'Content-Type': 'application/json','Accept': 'application/json'}
+        })
+        .then(data => data.json())
+        .then(data => setAccomodations(data))
+        .catch(error => console.log(error))
+    },[])
+
+    return (
+        <div className='container'>
+            <section className="grid-center">
+                <div className='element-filter'>
+                    <img src={image} id="image-home" alt="image_montagne'" />
+                </div>
+                <h1 id="catch-phrase">Chez vous, partout et ailleurs</h1>
+            </section>
+            <main>
+                {accomodations.map(accomodation => { 
+                    return (
+                        <Link to={`/accomodation/${accomodation.id}`}>
+                            <Card data={accomodation} key={accomodation.id} >
+
+                            </Card>
+                        </Link>
+                )})}
+            </main>
+        </div>
+    )
+}
+
+export default Home
