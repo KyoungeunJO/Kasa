78956c8d4af47476a51b6fd2108f29bb0b55757b
Kyoungeun JO <hke3259@naver.com>
2022-09-20 16:52:52 +0200

    Style: changed pictures asset for better looking

--- src/components/pages/Error.js
+++ src/components/pages/Error.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 function Error() {
     return (
-        <div className="error">
+        <div className="error-container">
             <h1>404</h1>
-            <h2>Oups! La page que vous demandez n'existe pas.</h2>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
             <Link to='/'>Retourner sur la page d’accueil</Link>
         </div>
     )
